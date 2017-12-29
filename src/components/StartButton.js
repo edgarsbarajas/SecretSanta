c7146850bf9f68c91c9ec6309dec6600038ec7db
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-12-28 22:24:32 -0800

    Give the scroll a little animation when click 'start'

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import animateScrollTo from 'animated-scroll-to';
 
 const StartButton = () => {
   return(
     <button
       className="button"
       style={{width: '100%', height: '50px', background: 'green', marginTop: '-4px', padding: '0', border: 'none', color: 'white'}}
-      onClick={() => {window.scrollTo(0, 0)}}>
+      onClick={() => {animateScrollTo(0);}}>
       START
     </button>
   )
