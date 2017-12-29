71c48b0616fb73713346e0fdc237dd28ef73ea8a
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-12-28 19:34:43 -0800

    Window scrolls up when the start button is clicked.

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const StartButton = () => {
   return(
-    <button className="button" style={{width: '100%', height: '50px', background: 'green', marginTop: '-4px', padding: '0', border: 'none', color: 'white'}}>
+    <button
+      className="button"
+      style={{width: '100%', height: '50px', background: 'green', marginTop: '-4px', padding: '0', border: 'none', color: 'white'}}
+      onClick={() => {window.scrollTo(0, 0)}}>
       START
     </button>
   )
