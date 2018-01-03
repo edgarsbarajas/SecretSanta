a8611f8bb211992b141e0fe95a9b7183e159d4d2
Edgar Barajas <edgarsbarajas@yahoo.com>
2018-01-02 21:35:35 -0800

    Install and implement react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Home from './components/Home'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home/>
+      <BrowserRouter>
+        <Route exact path='/' component={Home}/>
+      </BrowserRouter>
       </div>
     );
   }
